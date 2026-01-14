3870325bb5d06541efb4e5ece5687c4f8049850e
Ângelo Vidor <angelovidor5@gmail.com>
2026-01-14 01:09:16 -0300

    feat: add optional request/response headers logging and custom maskable fields

--- src/OwlLogs.Sdk/Options/OwlLogsOptions.cs
+++ src/OwlLogs.Sdk/Options/OwlLogsOptions.cs
@@ -4,31 +4,17 @@ public sealed class OwlLogsOptions
 {
     public bool LogRequestBody { get; set; } = false;
     public bool LogResponseBody { get; set; } = false;
-    public bool MaskSensitiveData { get; set; } = true;
+    public bool LogRequestHeaders { get; set; } = true; 
+    public bool LogResponseHeaders { get; set; } = true;
 
-
-    public int MaxBodySize { get; set; } = 32_000; // 32kb
-
-    public HashSet<string> MaskFields { get; set; } = new(StringComparer.OrdinalIgnoreCase)
-    {
-        "password",
-        "token",
-        "access_token",
-        "refresh_token",
-        "cpf",
-        "cnpj",
-        "accessToken"
-    };
-
-    public HashSet<string> MaskHeaders { get; set; } = new(StringComparer.OrdinalIgnoreCase)
-    {
-        "authorization",
-        "cookie",
-        "set-cookie"
-    };
+    public int MaxBodySize { get; set; } = 32_000;
 
     public HashSet<string> AllowedContentTypes { get; set; } = new(StringComparer.OrdinalIgnoreCase)
-    {
-        "application/json"
-    };
+        {
+            "application/json"
+        };
+
+    public OwlLogsMaskOptions Mask { get; } = new();
 }
+
+
